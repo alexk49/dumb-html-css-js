2a5de26b48992e15567159b0b58bf6c6eacd9c25
alexk49 <alexcode9@mailbox.org>
2024-04-17 09:05:24 +0100

    play around code from odin project js variables

--- hello.js
+++ hello.js
@@ -1,1 +1,43 @@
-console.log("hello again")
+// mainly from https://www.theodinproject.com/lessons/foundations-landing-page
+
+console.log("hello again");
+
+console.log(23 + 97);
+
+console.log(4 + 10 + 2 + 5 + 99 + 10);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+
+console.log(a);
+
+let b = 7 * a;
+
+console.log(b);
+
+const MAX = 57;
+
+let actual = MAX - 13;
+
+let percentage = actual / MAX;
+
+console.log(percentage);
+
+// define multiple variables on 1 line
+let user = "john", age = 25, message = "hello";
+
+// or
+let user2 = "alan",
+    age2 = 38,
+    message2 = "hello again";
+
+let admin = "admin", name3 = "name";
+
+newName = "John";
+
+admin = newName;
+
+console.log(admin)
+
+alert("everything on this page has run through the console (except this message)")
