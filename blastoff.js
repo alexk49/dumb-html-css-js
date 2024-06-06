239f54fe9500591d4897015adce4c5eb3018f6f8
alexk49 <alexcode9@mailbox.org>
2024-06-06 08:50:13 +0100

    first commit basic blast off button from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Looping_code

---
+++ blastoff.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    function startCountdown() {
+
+        const output = document.getElementById('output');
+        for (let countdown = 10; countdown >= 0; countdown -= 1) {
+            const para = document.createElement('p');
+            output.appendChild(para);
+            
+            if (countdown === 0) {
+                para.textContent = "Blast off!";
+            }
+            else {
+                para.textContent = countdown.toString();
+            }
+        }
+    }
+
+    const startButton = document.getElementById("start-button");
+
+    startButton.addEventListener("click", startCountdown);
+});
